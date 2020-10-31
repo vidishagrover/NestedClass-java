c505f0f9b5167f3f319b3a1565d906f7ed8f7458
vidishagrover <62940133+vidishagrover@users.noreply.github.com>
2020-10-31 18:53:17 +0530

    Add files via upload

---
+++ NestedClass.java
@@ -0,0 +1,20 @@
+
+class Outer {
+private int outer_x = 100;
+void test() {
+Inner inner = new Inner();
+inner.display();
+inner.hello();
+}
+class Inner {
+private void hello() {
+System.out.println("Private Method Hello() of Inner Class");
+}
+void display() {
+System.out.println("Display: outer_x = " + outer_x);
+
+Outer o = new Outer();
+System.out.println("Display: outer_x = " + o.outer_x);
+}
+}
+}
